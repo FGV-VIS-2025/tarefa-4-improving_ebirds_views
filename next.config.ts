401b78687e6430b87fee7c1bd29bec4e22400287
Gab-Mel <gabrielzinho.melo.10@gmail.com>
2025-05-02 17:42:08 -0300

    next aaa

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  basePath: '/tarefa-4-improving_ebirds_views',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
